2494b2b329152939c38a6b0864614894ffa58ae8
proxiermax <teethawat9438@gmail.com>
2025-12-09 19:00:39 +0700

    feat: re-count

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
       <h1>PROXIERMAX CI/CD</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          COUNT: {count}
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
